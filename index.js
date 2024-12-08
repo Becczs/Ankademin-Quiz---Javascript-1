4452ec1ace2d604923fe26204b438f0abf478060
Becczs <rebecca.anderssoncarlsson@gmail.com>
2024-12-08 18:38:37 +0100

    la frågorna i html istället för i js och skapat quizet

--- index.js
+++ index.js
@@ -1,75 +1,87 @@
-
-const themeToggle = document.querySelector('#themeToggle');
-const quizContainer = document.querySelector('.quizContainer');
-const startPage = document.querySelector('.startPage');
-const letsPlayBtn = document.querySelector('#letsPlayBtn');
+const startPage = document.querySelector(".startPage");
 const questionSection = document.querySelector('.questionSection');
 const nextBtn = document.querySelector('#nextBtn');
-const resultSection = document.querySelector('#resultSection');
-const score = document.querySelector('#score');
-const feedback = document.querySelector('#feedback');
+const letsPlayBtn = document.querySelector('#letsPlayBtn');
 const restartBtn = document.querySelector('#restartBtn');
-
-
-const questions = [
-  {
-    type: 'true-false',
-    question: 'JavaScript är ett programmeringsspråk.',
-    answer: true
-  },
-
-  {
-    type: 'radiobutton',
-    question: 'Vilket av följande är ett CSS-ramverk?',
-    options: ['React', 'Bootstrap', 'Node.js', 'MongoDB'],
-    answer: 1
-  },
-
-  {
-    type: "checkbox",
-    question: 'Vilka av följande är JavaScript-ramverk?',
-    options: ['React', 'Vue.js', 'Django', 'Angular'],
-    answer: [0, 1, 3]
-  }
-];
-
+const resultSection = document.querySelector('#resultSection');
+const points = document.querySelector('#score')
+const feedback = document.querySelector('#feedback')
 
 //ändra tema - darkmode/lightmode
 document.querySelector('#themeToggle').addEventListener('click', () => {
   //använder toogle-metoden för att skifta mellan hide() och show()
   document.body.classList.toggle('darkMode');
   /*kollar om klassen darkMode finns, gör en kort verision på if/else-sats och
-  om villkoret uppfylls så ändrar vi texten till lightMode*/
+  om villkoret uppfylls så ändras texten till lightMode*/
   document.querySelector('#themeToggle').innerHTML =
     document.body.classList.contains('darkMode') ? 'Light Mode' : 'Dark Mode';
 });
 
 
+// Starta quizet
 letsPlayBtn.addEventListener('click', () => {
-  //dölj startsidan och visa frågor samt next button.
-  letsPlayBtn.classList.add('hidden');
-  startPage.classList.add('hidden');
-  nextBtn.classList.remove('hidden');
+
+  startPage.classList.add('hidden')
+  questionSection.classList.remove('hidden')
+  nextBtn.classList.remove('hidden')
   manageQuestion();
 });
 
 
-let index = 0;
-let userScore = 0;
+let currentQuestion = 0;
+let score = 0;
+let totalPoints = 0;
 
-
-// nextBtn.addEventListener('click', () => {
-// manageQuestion();
-// })
-
-
-// const manageQuestion = () => {
-//   for (let index = 0; index <= questions.length; index++;)
-// }
-
+//hämta alla frågor
+let questions = document.querySelectorAll(".questionSection fieldset");
+// Funktion för att visa första frpgan
 function manageQuestion() {
-  const currentQuestion = questions[index];
-  questionSection.textContent = currentQuestion.question;
 
+  questions.forEach(question => question.classList.add('hidden'))
 
+  if (currentQuestion < questions.length) {
+    questions[currentQuestion].classList.remove('hidden')
+
+  } else {
+    showResults()
+  }
 }
+
+/*hämta alla frågor och räkna ut dina poäng,
+rätt på 1 fråga = 1 poäng*/
+function showResults() {
+  totalPoints = questions.length
+  questions.forEach(fieldset => {
+    const inputs = fieldset.querySelectorAll("input");
+    inputs.forEach(input => {
+      if (input.checked && input.value === "true") {
+        score++;
+      }
+    })
+  })
+
+  // Visa resultatet
+  resultSection.classList.remove('hidden')
+  points.textContent = `Du fick ${score} av ${totalPoints} rätt!`;
+  feedback.textContent = ``
+  nextBtn.classList.add('hidden')
+  restartBtn.classList.remove('hidden')
+};
+
+nextBtn.addEventListener('click', () => {
+  //gå vidare till nästa fråga
+  currentQuestion++
+  manageQuestion()
+});
+
+
+
+// Starta om quizet
+restartBtn.addEventListener('click', () => {
+
+  resultSection.classList.add('hidden')
+  startPage.classList.add('hidden')
+  nextBtn.classList.remove('hidden')
+  currentQuestion = 0
+  manageQuestion()
+});
